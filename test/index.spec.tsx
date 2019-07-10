7c01e81e43d448a18d01440b6ebc4930104f72e0
babie <babie.wildheart@gmail.com>
2019-07-10 21:25:39 +0900

    test: Reset the `document` variable before each tests

--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -7,10 +7,13 @@ declare var jsdom: JSDOM
 
 describe('preact-cap', (): void => {
   const myModule = rewire('../dist')
-  myModule.__set__('document', jsdom.window.document.defaultView.document)
-  const nodeToDOM = myModule.__get__('nodeToDOM')
+  beforeEach((): void => {
+    myModule.__set__('document', jsdom.window.document.defaultView.document)
+  })
 
   describe('nodeToDOM()', (): void => {
+    const nodeToDOM = myModule.__get__('nodeToDOM')
+
     it('when node has children', (): void => {
       const node = <meta name="description">Meta</meta>
       const el = document.createElement('meta')
