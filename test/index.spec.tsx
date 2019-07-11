fb567d7b0779047720d32f7e60d120ed84866211
babie <babie.wildheart@gmail.com>
2019-07-11 09:22:39 +0900

    test: Add `updateHead()` test when created

--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -40,54 +40,34 @@ describe('preact-cap', (): void => {
     })
   })
 
-  describe('updateTitle()', (): void => {
-    const updateTitle = myModule.__get__('updateTitle')
-
-    it('when nodes has 1 title', (): void => {
-      const nodes = [<title key="title1">Title1</title>]
-      updateTitle(nodes)
-
-      expect(document.title).toStrictEqual('Title1')
-    })
-
-    it('when nodes has 3 titles', (): void => {
-      const nodes = [
-        <title key="title1">Title1</title>,
-        <title key="title2">Title2</title>,
-        <title key="title3">Title3</title>
-      ]
-      updateTitle(nodes)
-
-      expect(document.title).toStrictEqual('Title1Title2Title3')
-    })
-  })
-
-  describe('updateOthers', (): void => {
-    const updateOthers = myModule.__get__('updateOthers')
+  describe('updateHead()', (): void => {
     const nodes = [
+      <title key="title">Default Title</title>,
       <meta
-        key="meta1"
+        key="meta"
         className="preact-cap"
         content="width=device-width,initial-scale=1"
         name="viewport"
       />,
       <base
-        key="base1"
+        key="base"
         className="preact-cap"
         href="http://localhost/"
         target="_self"
       />,
       <link
-        key="link1"
+        key="link"
         className="preact-cap"
         href="main.css"
         rel="stylesheet"
       />,
-      <style key="style1" className="preact-cap">
+      <style key="style" className="preact-cap">
         {'p { color: red; }'}
       </style>,
-      <script key="script1" className="preact-cap" src="main.js" defer />
+      <script key="script" className="preact-cap" src="main.js" defer />
     ]
+    const title = document.createElement('title')
+    title.textContent = 'Default Title'
     const meta = document.createElement('meta')
     meta.setAttribute('class', 'preact-cap')
     meta.setAttribute('content', 'width=device-width,initial-scale=1')
@@ -107,46 +87,82 @@ describe('preact-cap', (): void => {
     script.setAttribute('class', 'preact-cap')
     script.setAttribute('defer', 'true')
     script.setAttribute('src', 'main.js')
-    const elements = [meta, base, link, style, script]
+    const elements = [title, meta, base, link, style, script]
 
-    beforeEach((): void => {
-      updateOthers(nodes)
+    describe('updateTitle()', (): void => {
+      const updateTitle = myModule.__get__('updateTitle')
+
+      it('when nodes has 1 title', (): void => {
+        const titleNodes = [nodes[0]]
+        updateTitle(titleNodes)
+
+        expect(document.title).toStrictEqual('Default Title')
+      })
+
+      it('when nodes has 3 titles', (): void => {
+        const titleNodes = [
+          <title key="title1">Title1</title>,
+          <title key="title2">Title2</title>,
+          <title key="title3">Title3</title>
+        ]
+        updateTitle(titleNodes)
+
+        expect(document.title).toStrictEqual('Title1Title2Title3')
+      })
     })
 
-    it('when create', (): void => {
-      const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
+    describe('updateOthers()', (): void => {
+      const updateOthers = myModule.__get__('updateOthers')
+      const othersNodes = nodes.slice(1, nodes.length)
+      const othersElements = elements.slice(1, elements.length)
+
+      beforeEach((): void => {
+        updateOthers(othersNodes)
+      })
+
+      it('when created', (): void => {
+        const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
+
+        expect(tags).toStrictEqual(othersElements)
+      })
+
+      it('when removed', (): void => {
+        updateOthers([])
+        const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
+
+        expect(tags).toStrictEqual([])
+      })
+
+      it('when updated', (): void => {
+        const newNodes = othersNodes.slice(0, nodes.length)
+        newNodes[2] = (
+          <link
+            key="link1"
+            className="preact-cap"
+            href="another.css"
+            rel="stylesheet"
+          />
+        )
+        updateOthers(newNodes)
+        const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
+
+        const newElements = othersElements.slice(0, elements.length)
+        const newLink = document.createElement('link')
+        newLink.setAttribute('class', 'preact-cap')
+        newLink.setAttribute('href', 'another.css')
+        newLink.setAttribute('rel', 'stylesheet')
+        newElements[2] = newLink
+
+        expect(tags.sort()).toStrictEqual(newElements.sort())
+      })
+    })
+
+    const updateHead = myModule.__get__('updateHead')
+    it('when created', (): void => {
+      updateHead(nodes)
+      const tags = Array.from(document.head.children)
 
       expect(tags).toStrictEqual(elements)
-    })
-
-    it('when remove', (): void => {
-      updateOthers([])
-      const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
-
-      expect(tags).toStrictEqual([])
-    })
-
-    it('when update', (): void => {
-      const newNodes = nodes.slice(0, nodes.length)
-      newNodes[2] = (
-        <link
-          key="link1"
-          className="preact-cap"
-          href="another.css"
-          rel="stylesheet"
-        />
-      )
-      updateOthers(newNodes)
-      const tags = Array.from(document.head.querySelectorAll('.preact-cap'))
-
-      const newElements = elements.slice(0, elements.length)
-      const newLink = document.createElement('link')
-      newLink.setAttribute('class', 'preact-cap')
-      newLink.setAttribute('href', 'another.css')
-      newLink.setAttribute('rel', 'stylesheet')
-      newElements[2] = newLink
-
-      expect(tags.sort()).toStrictEqual(newElements.sort())
     })
   })
 
